8dac3600b906b9ce667ee7052cdbb663a00afbcf
Ben Evans <jedifreeman@gmail.com>
2014-10-09 13:50:30 -0700

    fixing index/show of snippet_files

--- app/controllers/admin/snippet_files_controller.rb
+++ app/controllers/admin/snippet_files_controller.rb
@@ -1,7 +1,16 @@
 class Admin::SnippetFilesController < Admin::ResourceController
-  def show;end
-  private
-  def load_models
-    SnippetFile.all
+  paginate_models
+  only_allow_access_to :index, :show, :new, :create, :edit, :update, :remove, :destroy,
+    :when => [:designer, :admin],
+    :denied_url => { :controller => 'admin/pages', :action => 'index' },
+    :denied_message => 'You must have designer privileges to perform this action.'
+  #def show;end
+  #private
+  #def load_models
+  #  SnippetFile.all
+  #end
+
+  responses do |r|
+    r.singular.default { }
   end
 end
